eaf731c71bec501452df09e509b9c1ce8b55d412
sparsh2 <sarodesparsh@gmail.com>
2020-01-03 13:04:52 +0530

    Added additional funcitonalities to Browse.py

--- Browse.py
+++ Browse.py
@@ -1,27 +1,35 @@
 from robobrowser import RoboBrowser
 import requests
-# import Login,Logout,Submit
 
-################################################
-## Example
+#######################################################################
+## Usage Example                                                      #
+#                                                                     #
+# import Browse as br                                                 #
+#                                                                     #
+# name = ''                                                           #
+# psw = ''                                                            #
+#                                                                     #
+# problem_code = 'EVIPRO'                                             #
+# contest_name = ''                                                   #
+# file_loc = 'sol.cpp'                                                #
+#                                                                     #
+# br.submit_solution(name, psw, problem_code, file_loc, contest_name) #
+#                                                                     #
+#######################################################################
 
-# name = ''
-# psw = ''
-#
-# problem_code = 'EVIPRO'
-#
-# br.Init()
-# br.Login(name,psw)
-# br.Submit(problem_code, 'sol.cpp')
-# br.Logout()
-#################################################
+def submit_solution(usern, psw, problem_code, file_loc, contest_name=""):
+    Init()
+    if Login(usern, psw) == -1:
+        return
+    Submit(problem_code, file_loc, contest_name)
+    Logout()
 
 
 def Init():
     global newSession,browser,loginStatus,url
     newSession = requests.session()
     newSession.headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
-    browser = RoboBrowser(history = True,session=newSession)
+    browser = RoboBrowser(history = True, session = newSession,parser = 'html.parser')
     loginStatus = 0
     url = 'https://www.codechef.com/'
 
@@ -45,7 +53,16 @@ def Login(usern, psw):
     browser.submit_form(login_form)
     loginStatus = 1
 
+
     # Add session limit funcitonality later
+
+    session_limit_form = browser.get_form(id = 'session-limit-page')
+    if session_limit_form is not None:
+        print('session limit reached.. Please logout and try again')
+        return -1
+
+    return 1
+
 
 
 
@@ -67,29 +84,22 @@ def Logout():
 
 def Submit(problem_code, file_loc, contest_name=""):
     global newSession,browser,loginStatus,url
+
+    # if(loginStatus == 0):
+    #     raise RuntimeError 'Not logged in'
+
     submit_url = url
     if(contest_name!=""):
         submit_url+=contest_name+'/'
     submit_url+='submit/'+problem_code
 
+
     browser.open(submit_url)
-
-    with open("subres.html", "w",encoding="utf-8") as text_file:
-        print(str(browser.select), file=text_file)
-
     submission_form = browser.get_form(id = 'problem-submission')
 
     if submission_form is None:
         temp_form = browser.get_form(id = 'do-not-show-ide-on-submit-form')
-
-        ## Double check
-        # if temp_form is None:
-        #     print('tempform is none, returning')
-        #     return
-
-
         browser.submit_form(temp_form)
-
         submission_form = browser.get_form(id = 'problem-submission')
 
 
